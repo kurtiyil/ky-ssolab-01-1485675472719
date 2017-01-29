0426d149ddeb2329cd9df324e7838791d7fbe37a
Kurtulus YILDIRIM <kurti_yil@yahoo.com>
2017-01-29 12:11:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -14,6 +17,22 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
+app.use(cookieParser());
+app.use(session({resave: 'true',saveUninitialized: 'true', secret: 'keyboard cat'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser(function(user, done) {
+    // done(null, user);
+    done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+    // user.className = "_User";
+    // user =  Parse.Object.fromJSON(user);
+    done(null, obj);
+});
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
